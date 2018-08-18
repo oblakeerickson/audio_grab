62be65ca4cfd50c6ed9513741eb93c9aa4879541
Blake Erickson <o.blakeerickson@gmail.com>
2018-08-18 09:17:39 -0600

    started adding some tests

--- app.rb
+++ app.rb
@@ -2,37 +2,60 @@ require 'mechanize'
 require 'fileutils'
 require 'yaml'
 
-config = YAML.load_file('config.yml')
+class AudioGrab
+  def initialize
+    @config = YAML.load_file('config.yml')
+  end
 
-# from the command line read:
-# 1. read youtube video url param
-# 2. use youtube-dl to download and convert it to mp3
-# 3. upload to overcast
-#
+  def download_and_convert_video(url)
+    system "youtube-dl -o 'process/%(title)s.%(ext)s' --restrict-filenames --extract-audio --audio-format mp3 #{url}"
+  end
 
-Dir.mkdir("process") unless File.exists?("process")
-Dir.mkdir("uploaded") unless File.exists?("uploaded")
+  def files
+    Dir["process/*.mp3"]
+  end
+
+  def mp3_file
+    self.files[0]
+  end
+end
 
 url = ARGV[0]
+audio_grab = AudioGrab.new
+audio_grab.download_and_convert_video(url)
+puts audio_grab.mp3_file
 
-system "youtube-dl -o 'process/%(title)s.%(ext)s' --restrict-filenames --extract-audio --audio-format mp3 #{url}"
-
-files = Dir["process/*.mp3"]
-file = files[0]
-
-a = Mechanize.new
-
-login_page = a.get('https://overcast.fm/login')
-
-my_page = login_page.form do |form|
-  form.email = config['overcast_username']
-  form.password = config['overcast_password']
-end.submit
-
-upload_page = a.click(my_page.link_with(:text => /ploads/))
-
-upload_page.form_with(:method => 'POST') do |upload_form|
-  upload_form.file_uploads.first.file_name = file
-end.submit
-
-FileUtils.mv(file, "uploaded/#{File.basename(file)}")
+#config = YAML.load_file('config.yml')
+#
+## from the command line read:
+## 1. read youtube video url param
+## 2. use youtube-dl to download and convert it to mp3
+## 3. upload to overcast
+##
+#
+#Dir.mkdir("process") unless File.exists?("process")
+#Dir.mkdir("uploaded") unless File.exists?("uploaded")
+#
+#url = ARGV[0]
+#
+#system "youtube-dl -o 'process/%(title)s.%(ext)s' --restrict-filenames --extract-audio --audio-format mp3 #{url}"
+#
+#files = Dir["process/*.mp3"]
+#file = files[0]
+#
+#a = Mechanize.new
+#
+#login_page = a.get('https://overcast.fm/login')
+#
+#my_page = login_page.form do |form|
+#  form.email = config['overcast_username']
+#  form.password = config['overcast_password']
+#end.submit
+#
+#upload_page = a.click(my_page.link_with(:text => /ploads/))
+#
+#upload_page.form_with(:method => 'POST') do |upload_form|
+#  upload_form.file_uploads.first.file_name = file
+#end.submit
+#
+#FileUtils.mv(file, "uploaded/#{File.basename(file)}")
